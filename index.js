ca8cbeef7060e0275602aa781120c463b3e51b7e
Alex Zdatny <alexzdatny@gmail.com>
2019-01-02 20:35:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,47 +1,52 @@
 $(document).ready(function (){
 });
 
-function displayError() {
+function displayError(error) {
   $('#errors').html("There was an error, please try again.");
 }
 
-function renderCommit(commit) {
-  return
-  `<li<h3>${commit.sha}</h3>
-  <p>${commit.commit.author.name}</p>
-  <p>${commit.commit.message}</p></li>`
+function displayRepositories(result) {
+  return `<strong>Name: </strong>${result.name}<br>
+         <strong>Description: </strong>${result.description}<br>
+         <strong>URL: </strong><a href="${result.html_url}">${result.html_url}</a><br>
+         <a href="#" data-repository="${result.name}" data-owner="${result.owner.login}"
+           onclick="showCommits(this)">Show Commits</a><br></br>`;
 }
 
-function renderCommits(data) {
-  let result = data.map((commit) => renderCommit(commit)).join('');
+function showCommits(ele) {
+  const repo  = ele.dataset.repository;
+  const owner = ele.dataset.owner;
+
+  $.get(`https://api.github.com/repos/${owner}/${repo}/commits`).done(function(data) {
+     $('#details').html(getCommits(data));
+   }).fail(function(error) {
+     displayError(error);
+  });
+}
+
+function getCommit(commit) {
+  return `<li><h3>Sha: ${commit.sha}</h3>
+         <img src="${commit.author.avatar_url}" alt="avatar" height="42"><br>
+         <strong>Name: </strong>${commit.commit.author.name}<br>
+         <strong>Login: </strong>${commit.author.login}<br>
+         <p><strong>Message: </strong>${commit.commit.message}</p></li>`;
+}
+
+function getCommits(data) {
+  const result = data.map( commit => getCommit(commit)).join('');
   return `<ul>${result}</ul>`;
 }
 
-function showCommits(el){
-  $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, data => {
-    $('#details').html(renderCommits(data))
-  }).fail(error => {
-    displayError()
-  })
-}
-
-function renderRepositories(result) {
-  return `
-  <li>
-  <h2><a href="${result.html_url}">${result.name}</a></h2>
-  <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
-  <p>${result.description}</p></li>`;
-}
-
-function displayRepositories(data) {
-  return data.items.map(result => renderRepositories(result));
+function getRepositories(data) {
+   return data.items.map( result => displayRepositories(result));
 }
 
 function searchRepositories() {
-  const searchTerms = document.getElementById('searchTerms').value
-  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
-    $("#results").html(displayRepositories(data));
-  }).fail(error => {
-    displayError()
-  })
+  const searchTerms = $('#searchTerms')[0].value;
+
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`).done(function(data) {
+     $('#results').html(getRepositories(data));
+   }).fail(function(error) {
+     displayError(error);
+  });
 }
