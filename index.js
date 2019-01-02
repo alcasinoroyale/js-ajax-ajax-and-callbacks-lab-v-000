ed286887d2fdcf7ed08653a0e132256dbc0b97a0
Alex Zdatny <alexzdatny@gmail.com>
2019-01-02 20:39:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ function displayError(error) {
   $('#errors').html("There was an error, please try again.");
 }
 
-function displayRepositories(result) {
-  return `<strong>Name: </strong>${result.name}<br>
-         <strong>Description: </strong>${result.description}<br>
-         <strong>URL: </strong><a href="${result.html_url}">${result.html_url}</a><br>
-         <a href="#" data-repository="${result.name}" data-owner="${result.owner.login}"
-           onclick="showCommits(this)">Show Commits</a><br></br>`;
+function renderRepositories(repo) {
+  return `
+  <li>
+    Name: ${result.name}<br>
+    Description: ${result.description}<br>
+    URL: <a href="${result.html_url}">${result.html_url}</a><br>
+    <a href="#" data-repository="${result.name}" data-owner="${result.owner.login}"
+    onclick="showCommits(this)">Show Commits</a><br></br>`;
 }
 
 function showCommits(ele) {
@@ -38,7 +40,7 @@ function getCommits(data) {
 }
 
 function getRepositories(data) {
-   return data.items.map( result => displayRepositories(result));
+   return data.items.map( result => renderRepositories(result));
 }
 
 function searchRepositories() {
