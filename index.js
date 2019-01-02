382651682d48ed0d4453be35842fd05c2d982533
Alex Zdatny <alexzdatny@gmail.com>
2019-01-02 19:36:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ $(document).ready(function (){
 function searchRepositories() {
   const searchTerms = document.getElementById('searchTerms').value
   $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function(data){
-    $("#results".html(showRepositories(data))
-  }).fail(error => {
+    $("#results").html(showRepositories(data));
+  }).fail(error =>
     displayError()
-  })
+  )
 }
