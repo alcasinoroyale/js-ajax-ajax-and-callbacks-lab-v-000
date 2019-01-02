772390fb58d2b37abf6ad1e0a8e7aeb2a7a8a5d2
Alex Zdatny <alexzdatny@gmail.com>
2019-01-02 20:17:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ $(document).ready(function (){
 function displayError() {
   $('#errors').html("There was an error, please try again.");
 }
+
+function renderCommit(commit) {
+  return
+  `<li<h3>${commit.sha}</h3>
+  <p>${commit.commit.author.name}</p>
+  <p>${commit.commit.message}</p></li>`
+}
